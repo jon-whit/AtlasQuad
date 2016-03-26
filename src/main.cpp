2f09a1edae830431168b6db6836adda307824076
jon-whit <jon.b.whitaker@gmail.com>
2016-03-26 00:28:07 -0600

    Cleaned up src directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,45 @@
 #include "mbed.h"
+#include "motors.h"
+#include "ITG3200.h"
+#include "pid.h"
 
-int main(void)
+Serial pc(SERIAL_TX, SERIAL_RX); // TX, RX
+//ITG3200 gyro(PB_9, PB_8);
+
+int setpoint = 0;
+
+int mspeed1 = MOTOR_MIN, mspeed2 = MOTOR_MIN;
+int mspeed3 = MOTOR_MIN, mspeed4 = MOTOR_MIN;
+
+int main()
 {
+    pc.printf("Initializing Motors...\r\n");
+    InitMotors();
+    
+    pc.printf("Initializing IMU...\r\n");
+    
+    pc.printf("Arming Motors...\r\n");
+    ArmMotors();
     
     while (1)
     {
+        char c = pc.getc();
+        if(c == 'u') {
+            mspeed1 += 10;
+            mspeed2 += 10;
+            mspeed3 += 10;
+            mspeed4 += 10;
+            UpdateMotors(&mspeed1, &mspeed2, &mspeed3, &mspeed4);
+            pc.printf("%i us\r\n", mspeed1);
+        }
         
+        if(c == 'd') {
+            mspeed1 -= 10;
+            mspeed2 -= 10;
+            mspeed3 -= 10;
+            mspeed4 -= 10;
+            UpdateMotors(&mspeed1, &mspeed2, &mspeed3, &mspeed4);
+            pc.printf("%i us\r\n", mspeed1);
+        } 
     }
-    
-    return 0;
 }

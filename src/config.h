9c0b2d5a94d65df4d1e39edebf58c3fd8bb7c252
Danial Ebling <danial.ebling@utah.edu>
2016-03-26 18:27:07 -0600

    Added XBee library to the project

---
+++ src/config.h
@@ -0,0 +1,15 @@
+// required for the XBee library
+
+// enable logging through the DigiLogger
+//#define ENABLE_LOGGING
+
+// internal circular buffer; currently an arbitrary value
+#define FRAME_BUFFER_SIZE 15
+
+// limit max frame length
+#define MAX_FRAME_PAYLOAD_LEN 128
+
+// set default timeout in milliseconds
+#define SYNC_OPS_TIMEOUT_MS 2000
+
+// XBee pin definitions
